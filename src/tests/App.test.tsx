2d507bf59c718e0ae170c48b01db7c9c9d2b4db3
pauloivoandrade <pauloivoasantos@gmail.com>
2023-09-12 11:15:22 -0300

    Feat: Test home link and About link

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,20 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Teste o componente <App.tsx />', () => {
+  test('Teste se o topo da aplicação contém link deve ter o texto Home', () => {
+    render(<App />, { wrapper: BrowserRouter });
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('Teste se o topo da aplicação contém link deve ter o texto About', () => {
+    render(<App />, { wrapper: BrowserRouter });
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('', () => {});
+  test('', () => {});
+});
